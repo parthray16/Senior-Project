0ce2d8cd21940d35fbead7f9e96399857af6794f
Parth Ray <59034844+parthray16@users.noreply.github.com>
2022-04-30 13:20:52 -0700

    Add files via upload

---
+++ data_from_video.py
@@ -0,0 +1,40 @@
+import cv2
+import sys
+import os
+import math
+
+def main():
+    # Open video that you want to extract frames from
+    cap = cv2.VideoCapture('Videos/' + sys.argv[1])
+    i = 0
+    
+    videoName = sys.argv[1].split('.')[0]
+
+    os.mkdir('Frames/' + sys.argv[1])
+    pathOut = 'Frames/' + sys.argv[1] + '/' + videoName
+
+    # Get framerate every four seconds
+    frameRate = math.floor(cap.get(5))
+    
+    while(cap.isOpened()):
+        # Current frame number
+        frameId = cap.get(1)
+
+        ret, frame = cap.read()
+        
+        # This condition prevents from infinite looping
+        # incase video ends.
+        if ret == False:
+            break
+        
+        # Save Frame every four seconds into disk using imwrite method
+        if (frameId % frameRate == 0):
+            cv2.imwrite(pathOut + str(i) + '.jpg', frame)
+            i += 1
+    
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
